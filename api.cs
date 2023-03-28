3ae9c9475762eb09cba378e1e1f06377df4f272c
liadk07 <91607695+liad07@users.noreply.github.com>
2023-03-28 19:37:13 +0300

    Add files via upload

---
+++ api.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace shabatkodesh
+{
+    public class api
+    {
+        public api()
+        {
+
+        }
+
+        public async Task<string>  GetContent(string url)
+    {
+            await Task.Delay(3000);
+
+            var client = new HttpClient();
+        var response = await client.GetAsync(url) ;
+        var content = await response.Content.ReadAsStringAsync();
+            // Process the response content
+         return content;
+    }
+}
+}
